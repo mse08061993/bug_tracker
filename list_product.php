005bc8e9eab0a15dd2221bae97bb42a562570475
Marchenko Sergey <mse08061993@gmail.com>
2024-01-30 15:30:21 +0300

    Add script to list all products

---
+++ list_product.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once 'bootstrap.php';
+
+$productRepository = $entityManager->getRepository(Product::class);
+$products = $productRepository->findAll();
+foreach ($products as $product) {
+    echo $product->getName() . "\n";
+}
